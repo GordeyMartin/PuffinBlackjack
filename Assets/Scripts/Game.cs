96b287cc98300c7409dd5191ea229f3b92fd82c5
Gordey Danilochkin <gordanshow@gmail.com>
2024-09-20 19:24:21 -0500

    Dealers moves

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -8,14 +8,25 @@ public class Game : MonoBehaviour
 {
     public GameObject Player_CardPrefab;
 
-    public TextMeshProUGUI scoreText;
-
-    //Arrays of cards and values;
+    //Arrays of cards and values of the Player;
     public List<Sprite> cardSprites;
     public List<int> cardValues;
 
+    //Arrays of cards and values of the Dealer;
+    public List<Sprite> cardSpritesDealer;
+    public List<int> cardValuesDealer;
+
+    //Score of the player
     public int score = 0;
+    public TextMeshProUGUI scoreText;
+
+    //Score of the dealer
+    public int dealerScore = 0;
+    public TextMeshProUGUI dealerText;
+
+    //Number of cards played
     public int numCards = 0;
+    public int numCardsDealer = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -28,7 +39,8 @@ public class Game : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        scoreText.text = ""+score;
+        scoreText.text = "" + score;
+        dealerText.text = "" + dealerScore;
     }
 
     //If clicked hit
@@ -39,10 +51,10 @@ public class Game : MonoBehaviour
             SpawnCard();
         }
     }
-
+    //If Player stays, he makes dealer move.
     public void Stay()
     {
-
+        DealersMove();
     }
 
     public void SpawnCard()
@@ -65,12 +77,45 @@ public class Game : MonoBehaviour
             cardValues.RemoveAt(randomIndex);
 
             numCards++;
+            Debug.Log(numCards);
         }
     }
-
+    //Button to go back to menu
     public void LoadMenu()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void DealersMove()
+    {
+        while (dealerScore < score && dealerScore <= 21)
+        {
+            spawnEnemyCard();
+        }
+    }
+
+    public void spawnEnemyCard()
+    {
+        if (numCardsDealer <= 3)
+        {
+            Vector3 spawnPosition = new Vector3(500 + numCardsDealer * 250f, 800f, 0f);
+
+            GameObject cardObject = Instantiate(Player_CardPrefab, spawnPosition, Quaternion.identity);
+
+            int randomIndex = Random.Range(0, cardSpritesDealer.Count);
+
+            Card card = cardObject.GetComponent<Card>();
+            card.SetCard(cardSpritesDealer[randomIndex], cardValuesDealer[randomIndex]);
+
+            dealerScore += cardValuesDealer[randomIndex];
+            Debug.Log(dealerScore);
+
+            cardSpritesDealer.RemoveAt(randomIndex);
+            cardValuesDealer.RemoveAt(randomIndex);
+
+            numCardsDealer++;
+            Debug.Log(numCardsDealer);
+        }
     }
 
 }
